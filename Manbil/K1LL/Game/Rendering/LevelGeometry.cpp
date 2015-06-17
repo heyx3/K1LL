3ed3d693135561bd809bbe880353ec67271832ea
heyx3 <manning.w@husky.neu.edu>
2015-06-16 21:56:12 -0400

    Fixed wall geometry generation

--- Manbil/K1LL/Game/Rendering/LevelGeometry.cpp
+++ Manbil/K1LL/Game/Rendering/LevelGeometry.cpp
@@ -15,58 +15,51 @@ LevelGeometry::LevelGeometry(const Array2D<BlockTypes>& levelGrid, std::string& 
     std::vector<MyVert> vertices;
     std::vector<unsigned int> indices;
 
-    float ceilHeight = (float)Constants::Instance.CeilingHeight;
+    float ceilHeight = Constants::Instance.CeilingHeight;
 
-    //The six sides of the level bounds.
-    std::vector<PrimitiveGenerator::CubemapVertex> vs;
-    PrimitiveGenerator::GenerateCubemapCube(vs, indices, false, true,
-                                            Vector3f(),
-                                            Vector3f((float)levelGrid.GetWidth(),
-                                                     (float)levelGrid.GetHeight(),
-                                                     ceilHeight));
-    for (unsigned int i = 0; i < vs.size(); ++i)
-    {
-        vertices.push_back(MyVert(vs[i].Pos, vs[i].Normal));
-    }
+    //For every empty space, build the walls that border it.
 
-    //Now build each exposed face of each wall.
-    for (Vector2u counter; counter.y < levelGrid.GetHeight(); ++counter.y)
-    {
-        for (counter.x = 0; counter.x < levelGrid.GetWidth(); ++counter.x)
-        {
-            if (levelGrid[counter] == BT_WALL)
-            {
 #define ADD_WALL(v1, v2, v3, v4, normal) \
     vertices.push_back(MyVert(v1, normal)); \
     vertices.push_back(MyVert(v2, normal)); \
     vertices.push_back(MyVert(v3, normal)); \
     vertices.push_back(MyVert(v4, normal)); \
+    indices.push_back(vertices.size() - 4); \
     indices.push_back(vertices.size() - 3); \
     indices.push_back(vertices.size() - 2); \
-    indices.push_back(vertices.size() - 1); \
-    indices.push_back(vertices.size() - 3); \
-    indices.push_back(vertices.size() - 1); \
-    indices.push_back(vertices.size() - 0);
+    indices.push_back(vertices.size() - 4); \
+    indices.push_back(vertices.size() - 2); \
+    indices.push_back(vertices.size() - 1);
 
-                if (levelGrid[counter.LessX()] != BT_WALL)
+    Vector2f sizeF = ToV2f(levelGrid.GetDimensions());
+
+    for (Vector2u counter; counter.y < levelGrid.GetHeight(); ++counter.y)
+    {
+        for (counter.x = 0; counter.x < levelGrid.GetWidth(); ++counter.x)
+        {
+            assert(levelGrid[counter] != BT_DOORWAY);
+
+            if (levelGrid[counter] == BT_NONE || levelGrid[counter] == BT_SPAWN)
+            {
+                if (counter.x == 0 || levelGrid[counter.LessX()] == BT_WALL)
                 {
                     float minY = (float)counter.y,
                           maxY = minY + 1.0f;
                     float x = (float)counter.x;
                     ADD_WALL(Vector3f(x, minY, 0.0f), Vector3f(x, maxY, 0.0f),
                              Vector3f(x, maxY, ceilHeight), Vector3f(x, minY, ceilHeight),
-                             Vector3f(-1.0f, 0.0f, 0.0f))
+                             Vector3f(1.0f, 0.0f, 0.0f))
                 }
-                if (levelGrid[counter.LessY()] != BT_WALL)
+                if (counter.y == 0 || levelGrid[counter.LessY()] == BT_WALL)
                 {
                     float minX = (float)counter.x,
                           maxX = minX + 1.0f;
                     float y = (float)counter.y;
                     ADD_WALL(Vector3f(minX, y, 0.0f), Vector3f(maxX, y, 0.0f),
                              Vector3f(maxX, y, ceilHeight), Vector3f(minX, y, ceilHeight),
-                             Vector3f(0.0f, -1.0f, 0.0f))
+                             Vector3f(0.0f, 1.0f, 0.0f))
                 }
-                if (levelGrid[counter.MoreX()] != BT_WALL)
+                if (counter.x == levelGrid.GetWidth() - 1 || levelGrid[counter.MoreX()] == BT_WALL)
                 {
                     float minY = (float)counter.y,
                           maxY = minY + 1.0f;
@@ -75,7 +68,7 @@ LevelGeometry::LevelGeometry(const Array2D<BlockTypes>& levelGrid, std::string& 
                              Vector3f(x, maxY, ceilHeight), Vector3f(x, minY, ceilHeight),
                              Vector3f(1.0f, 0.0f, 0.0f))
                 }
-                if (levelGrid[counter.MoreY()] != BT_WALL)
+                if (counter.y == levelGrid.GetHeight() - 1 || levelGrid[counter.MoreY()] == BT_WALL)
                 {
                     float minX = (float)counter.x,
                           maxX = minX + 1.0f;
@@ -87,6 +80,14 @@ LevelGeometry::LevelGeometry(const Array2D<BlockTypes>& levelGrid, std::string& 
             }
         }
     }
+
+    //Add the floors/ceiling.
+    ADD_WALL(Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, sizeF.y, 0.0f),
+             Vector3f(sizeF.x, sizeF.y, 0.0f), Vector3f(sizeF.x, 0.0f, 0.0f),
+             Vector3f(0.0f, 0.0f, 1.0f));
+    ADD_WALL(Vector3f(0.0f, 0.0f, ceilHeight), Vector3f(0.0f, sizeF.y, ceilHeight),
+             Vector3f(sizeF.x, sizeF.y, ceilHeight), Vector3f(sizeF.x, 0.0f, ceilHeight),
+             Vector3f(0.0f, 0.0f, -1.0f));
 
     //Finally, generate the mesh data buffers.
     mesh.SubMeshes.push_back(MeshData(false, PrimitiveTypes::PT_TRIANGLE_LIST));
@@ -107,29 +108,33 @@ LevelGeometry::LevelGeometry(const Array2D<BlockTypes>& levelGrid, std::string& 
     vUse.EnableFlag(MaterialUsageFlags::DNF_USES_VIEWPROJ_MAT);
 
     std::string vShader = MaterialConstants::GetVertexHeader(vOuts, myVertAttrs, vUse) +
-"\nvoid main() \n\
-{ \n\
-    fIn_WorldPos = " + vIn_Pos + "; \n\
-    fIn_WorldNormal = " + vIn_Normal + "; \n\
-    gl_Position = " + u_vp + " * vec4(" + vIn_Pos + ", 1.0); \n\
-}";
+R"(
+void main()
+{
+    //Standard vertex shader for vertices that are in world space already.
+    fIn_WorldPos = )" + vIn_Pos + R"(;
+    fIn_WorldNormal = )" + vIn_Normal + R"(;
+    gl_Position = )" + u_vp + " * vec4(" + vIn_Pos + R"( , 1.0);
+})";
 
     std::string fIns = "in vec3 fIn_WorldPos;\nin vec3 fIn_WorldNormal;",
                 fOuts = "out vec4 fOut_Color;";
 
     std::string fShader = MaterialConstants::GetFragmentHeader(fIns, fOuts, MaterialUsageFlags()) +
-"\nvoid main() \n\
-{ \n\
-    const float gridSizeScale = 4.0; \n\
-    vec3 tangent = cross(fIn_WorldNormal, vec3(0.0, 0.0, 1.0)); \n\
-    \n\
-    //For each vector (up and tangent), get a value indicating from 0 to 1 \n\
-    //    how close this fragment is to a grid line in that direction. \n\
-    vec2 gridVals = fract(gridSizeScale * vec2(fIn_WorldPos.z, dot(fIn_WorldPos, tangent))); \n\
-    gridVals = pow(2.0 * abs(gridVals - 0.5), 2.5); \n\
-    \n\
-    fOut_Color = vec4(vec3(fIn_WorldPos.z / 16.0) + max(gridVals.x, gridVals.y), 1.0); \n\
-}";
+R"(
+void main()
+{
+    //For each vector (up and tangent), get a value indicating from 0 to 1
+    //    how close this fragment is to a grid line in that direction.
+
+    const float gridSizeScale = 4.0;
+    vec3 tangent = cross(fIn_WorldNormal, vec3(0.0, 0.0, 1.0));
+   
+    vec2 gridVals = fract(gridSizeScale * vec2(fIn_WorldPos.z, dot(fIn_WorldPos, tangent)));
+    gridVals = pow(2.0 * abs(gridVals - vec2(0.5)), vec2(5.0));
+    
+    fOut_Color = vec4(vec3((fIn_WorldPos.z / 16.0) + max(gridVals.x, gridVals.y)), 1.0);
+})";
 
     mat = new Material(vShader, fShader, params, myVertAttrs, BlendMode::GetOpaque(), err);
     if (!err.empty())
@@ -154,6 +159,7 @@ LevelGeometry::~LevelGeometry(void)
 
 bool LevelGeometry::Update(Level* theLevel, float elapsedTime)
 {
+    //Never destroy this actor.
     return false;
 }
 void LevelGeometry::Render(Level* theLevel, float elapsedTime, const RenderInfo& info)
